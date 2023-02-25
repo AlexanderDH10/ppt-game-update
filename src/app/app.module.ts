2d4d3f6ef8193374a75838c9ebc622f21e6cd64a
AlexanderDH10 <you@example.com>
2023-02-25 15:07:38 -0600

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { RulesModalComponent } from './components/rules-modal/rules-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    MainComponent,
+    RulesModalComponent
   ],
   imports: [
     BrowserModule
